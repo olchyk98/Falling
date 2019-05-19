031063e4d9ba3f7d5e072770be212676dc1e62e0
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-05-19 12:35:59 +0200

    Assets loader

--- public/game.js
+++ public/game.js
@@ -1,7 +1,64 @@
+const spreadID = a => {
+    let i = 0;
 
+    for(let ma of Object.keys(a)) a[ma].id = i++;
+
+    return a;
+}
+
+// ABLOCK - ACTION BLOCK
+const gameAssets = window.gameAssets = spreadID({
+    "GRASS_BLOCK": {
+        type: "LOADABLE_MODEL",
+        murl: './assets/blocks/grass.png',
+        output: null
+    },
+    "SPIKES_ABLOCK": {
+        type: "LOADABLE_NOKEYS_MODELS_PACK",
+        murl: [
+            './assets/other/spikes/1.png',
+            './assets/other/spikes/2.png',
+            './assets/other/spikes/3.png',
+            './assets/other/spikes/4.png'
+        ],
+        output: null
+    }
+});
+const gameInfo = window.gameInfo = {}
+
+/*
+	0 - void
+	1 - grass block
+	2 - spikes
+*/
+const map = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 function preload() {
-    // ...
+    // func@: Get assets by type
+    const gt = a => Object.values(gameAssets).filter(io => io.type === a);
+
+    // Load single models
+    for(let ma of gt("LOADABLE_MODEL")) {
+        ma.output = loadImage(ma.murl);
+    }
+
+    // Load images pack without keys
+    for(let ma of gt("LOADABLE_NOKEYS_MODELS_PACK")) {
+        ma.output = [];
+        for(let mk of ma.murl) ma.output.push(loadImage(mk));
+    }
 }
 
 function setup() {
